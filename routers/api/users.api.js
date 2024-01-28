0f09f049ba407f87b0fd158ea8b0f66ce268c323
Evander Oktapian <evandertambunan@gmail.com>
2024-01-28 22:48:16 +0700

    Hapus semua file dan direktori

--- routers/api/users.api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const users = express.Router()
-const UserController = require('../../controllers/users.controller')
-const usersController = new UserController
-
-users.post("/users/login",usersController.login)
-users.post("/users/registration",usersController.addUser)
-
-module.exports = users
--- tesajah.js
+++
@@ -1,5 +0,0 @@
-const data1 = [{ name: "evander", age: 25 }];
-const data2 = { name: "oktapian", age: 30 };
-
-data1.push(data2)
-console.log(data1)
